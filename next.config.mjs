20377f3b6bd5e98908b31b3a8b78fc60209fdd07
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-11-18 10:48:46 +0500

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  env: {
+    NEXTAUTH_URL: "https://new-catalog.vercel.app/",
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_SECRET: "Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main-page",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
