410f31eb7772089e4c4eff86742002c9a3081398
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-11-24 16:51:25 +0500

    fixed

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -6,13 +6,14 @@ import { TelegramIcon } from "@/components/icons/social-media/telegram";
 import { InstagramIcon } from "@/components/icons/social-media/instagram";
 import { FacebookIcon } from "@/components/icons/social-media/facebook";
 import { YoutubeIcon } from "@/components/icons/social-media/youtube";
+import Reveal from "@/components/reveal";
 
 const Index = () => {
   return (
     <div className="bg-[#F7F7F7]">
       <Header />
 
-      <main className="container mb-[46px]">
+      <main className="container ">
         <section className="mt-[16px] flex items-center space-x-[12px] font-gilroy">
           <Link href={"/"} className="text-[#262D33] text-sm font-semibold">
             Bosh sahifa
@@ -93,7 +94,67 @@ const Index = () => {
               </div>
             </div>
 
-            <div className="font-gilroy bg-white p-[16px] border border-[#E0E2F0] rounded-[20px] col-span-7"></div>
+            <div className="font-gilroy bg-white p-[16px] border border-[#E0E2F0] rounded-[20px] col-span-7">
+              <h3 className="text-[24px] font-bold mb-[20px]">
+                Ariza qoldirish
+              </h3>
+
+              <form>
+                <div className="flex gap-x-[20px]">
+                  <div className="w-full">
+                    <label className="text-[#718096] ">FIO</label> <br />
+                    <input
+                      type="text"
+                      placeholder="Toʻliq ismingizni kiriting"
+                      className="py-[14px] px-[16px] border border-[#DDE3EA] rounded-[12px] w-full mt-[10px]"
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <label className="text-[#718096]">Telefon raqam</label>{" "}
+                    <br />
+                    <input
+                      type="tel"
+                      placeholder="+998"
+                      className="py-[14px] px-[16px] border border-[#DDE3EA] rounded-[12px] w-full  mt-[10px]"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full mt-[20px]">
+                  <label className="text-[#718096]">
+                    Murojaatingiz mavzusi
+                  </label>{" "}
+                  <br />
+                  <textarea
+                    placeholder="Kiriting"
+                    className="py-[14px] px-[16px] border border-[#DDE3EA] rounded-[12px] w-full  mt-[10px]"
+                  ></textarea>
+                </div>
+
+                <button className="font-semibold text-white py-[12px] px-[24px] bg-[#0256BA] rounded-[12px] mt-[20px]">
+                  Yuborish
+                </button>
+              </form>
+            </div>
+
+            <div className="col-span-12 mt-[60px]">
+              {" "}
+              <Reveal>
+                <div
+                  className=" min-h-[164px] w-full bg-center bg-no-repeat
+            bg-cover rounded-[20px] flex items-center justify-center"
+                  style={{ backgroundImage: `url(/images/address.png)` }}
+                >
+                  <Link
+                    href={"https://maps.app.goo.gl/nxPwsjmq6vi6hCPa7"}
+                    className="py-[14px] px-[28px] text-sm font-medium font-gilroy text-white bg-[#0256BA] rounded-[12px]"
+                  >
+                    Google Maps ni ochish
+                  </Link>
+                </div>
+              </Reveal>
+            </div>
           </div>
         </section>
       </main>
