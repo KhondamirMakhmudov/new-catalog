c97e07a44fe1104fce56f24f0bd42b5b17c93f1f
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-11-26 19:15:15 +0500

    fixed

--- src/components/model-projects/medicine/index.jsx
+++ src/components/model-projects/medicine/index.jsx
@@ -5,6 +5,8 @@ import { URLS } from "@/constants/url";
 import { motion } from "framer-motion";
 import { get } from "lodash";
 import { useState } from "react";
+import ContentLoader from "@/components/loader/content-loader";
+import ProjectPagination from "@/components/pagination/loyihalar-pagination";
 
 const MedicineProject = () => {
   const [limit] = useState(24);
@@ -29,61 +31,65 @@ const MedicineProject = () => {
 
   return (
     <div>
-      <motion.table
-        className="w-full border-collapse border-[#D7D9E7]"
-        initial={{ opacity: 0, translateY: "30px" }}
-        animate={{ opacity: 1, translateY: "0" }}
-        transition={{ duration: 0.4 }}
-      >
-        <thead className="text-black text-start rounded-[10px]">
-          <tr className="rounded-[10px]">
-            <th
-              className={
-                "px-4 py-2 text-[10px] rounded-tl-[10px] bg-white  text-gray-900  font-bold "
-              }
-            >
-              №
-            </th>
-            <th className=" text-[10px]  text-start  bg-white text-gray-900  font-bold ">
-              Mahsulot nomi
-            </th>
-            <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
-              Mahsulot Kodi
-            </th>
-            <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
-              O&apos;lchov Birligi
-            </th>
-            <th className=" text-start text-[10px] rounded-tr-[10px]  bg-white text-gray-900  font-bold ">
-              Narxi
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {get(medicine, "data.results", []).map((item, index) => (
-            <tr
-              key={get(item, "id")}
-              className="text-sm odd:bg-[#EDF4FC] even:bg-white"
-            >
-              <td className=" font-medium text-xs py-[10px]  text-center">
-                {index + 1}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "name")}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "code")}
-              </td>
-              <td className=" font-medium text-xs py-[10px] text-center">
-                {get(item, "measure")}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "price")}
-              </td>
+      {isLoading || isFetching ? (
+        <ContentLoader />
+      ) : (
+        <motion.table
+          className="w-full border-collapse border-[#D7D9E7]"
+          initial={{ opacity: 0, translateY: "30px" }}
+          animate={{ opacity: 1, translateY: "0" }}
+          transition={{ duration: 0.4 }}
+        >
+          <thead className="text-black text-start rounded-[10px]">
+            <tr className="rounded-[10px]">
+              <th
+                className={
+                  "px-4 py-2 text-[10px] rounded-tl-[10px] bg-white  text-gray-900  font-bold "
+                }
+              >
+                №
+              </th>
+              <th className=" text-[10px]  text-start  bg-white text-gray-900  font-bold ">
+                Mahsulot nomi
+              </th>
+              <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
+                Mahsulot Kodi
+              </th>
+              <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
+                O&apos;lchov Birligi
+              </th>
+              <th className=" text-start text-[10px] rounded-tr-[10px]  bg-white text-gray-900  font-bold ">
+                Narxi
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </motion.table>
+          </thead>
+
+          <tbody>
+            {get(medicine, "data.results", []).map((item, index) => (
+              <tr
+                key={get(item, "id")}
+                className="text-sm odd:bg-[#EDF4FC] even:bg-white"
+              >
+                <td className=" font-medium text-xs py-[10px]  text-center">
+                  {index + 1}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "name")}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "code")}
+                </td>
+                <td className=" font-medium text-xs py-[10px] text-center">
+                  {get(item, "measure")}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "price")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </motion.table>
+      )}
       <div className="w-full h-[1px] text-[#E2E2EA] "></div>
       <div className="py-[20px] px-[24px] bg-white rounded-br-[12px] rounded-bl-[12px] flex items-center justify-between">
         <div>
@@ -94,7 +100,10 @@ const MedicineProject = () => {
         </div>
 
         <div>
-          <Pagination pageCount={pageCount} onPageChange={handlePageClick} />
+          <ProjectPagination
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+          />
         </div>
       </div>
     </div>
--- src/components/model-projects/school/index.jsx
+++ src/components/model-projects/school/index.jsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
-import Pagination from "@/components/pagination";
 import useGetQuery from "@/hooks/api/useGetQuery";
 import { KEYS } from "@/constants/key";
 import { URLS } from "@/constants/url";
 import { motion } from "framer-motion";
 import { get } from "lodash";
 import { useState } from "react";
+import ContentLoader from "@/components/loader/content-loader";
+import ProjectPagination from "@/components/pagination/loyihalar-pagination";
 
 const SchoolProject = () => {
   const [limit] = useState(24);
@@ -31,61 +30,65 @@ const SchoolProject = () => {
 
   return (
     <div>
-      <motion.table
-        className="w-full border-collapse border-[#D7D9E7]"
-        initial={{ opacity: 0, translateY: "30px" }}
-        animate={{ opacity: 1, translateY: "0" }}
-        transition={{ duration: 0.4 }}
-      >
-        <thead className="text-black text-start rounded-[10px]">
-          <tr className="rounded-[10px]">
-            <th
-              className={
-                "px-4 py-2 text-[10px] rounded-tl-[10px] bg-white  text-gray-900  font-bold "
-              }
-            >
-              №
-            </th>
-            <th className=" text-[10px]  text-start  bg-white text-gray-900  font-bold ">
-              Mahsulot nomi
-            </th>
-            <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
-              Mahsulot Kodi
-            </th>
-            <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
-              O&apos;lchov Birligi
-            </th>
-            <th className=" text-start text-[10px] rounded-tr-[10px]  bg-white text-gray-900  font-bold ">
-              Narxi
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {get(school, "data.results", []).map((item, index) => (
-            <tr
-              key={get(item, "id")}
-              className="text-sm odd:bg-[#EDF4FC] even:bg-white"
-            >
-              <td className=" font-medium text-xs py-[10px]  text-center">
-                {index + 1}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "name")}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "code")}
-              </td>
-              <td className=" font-medium text-xs py-[10px] text-center">
-                {get(item, "measure")}
-              </td>
-              <td className=" font-medium text-xs py-[10px]">
-                {get(item, "price")}
-              </td>
+      {isLoadingSchool || isFetchingSchool ? (
+        <ContentLoader />
+      ) : (
+        <motion.table
+          className="w-full border-collapse border-[#D7D9E7]"
+          initial={{ opacity: 0, translateY: "30px" }}
+          animate={{ opacity: 1, translateY: "0" }}
+          transition={{ duration: 0.4 }}
+        >
+          <thead className="text-black text-start rounded-[10px]">
+            <tr className="rounded-[10px]">
+              <th
+                className={
+                  "px-4 py-2 text-[10px] rounded-tl-[10px] bg-white  text-gray-900  font-bold "
+                }
+              >
+                №
+              </th>
+              <th className=" text-[10px]  text-start  bg-white text-gray-900  font-bold ">
+                Mahsulot nomi
+              </th>
+              <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
+                Mahsulot Kodi
+              </th>
+              <th className=" text-start text-[10px]   bg-white text-gray-900  font-bold ">
+                O&apos;lchov Birligi
+              </th>
+              <th className=" text-start text-[10px] rounded-tr-[10px]  bg-white text-gray-900  font-bold ">
+                Narxi
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </motion.table>
+          </thead>
+
+          <tbody>
+            {get(school, "data.results", []).map((item, index) => (
+              <tr
+                key={get(item, "id")}
+                className="text-sm odd:bg-[#EDF4FC] even:bg-white"
+              >
+                <td className=" font-medium text-xs py-[10px]  text-center">
+                  {index + 1}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "name")}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "code")}
+                </td>
+                <td className=" font-medium text-xs py-[10px] text-center">
+                  {get(item, "measure")}
+                </td>
+                <td className=" font-medium text-xs py-[10px]">
+                  {get(item, "price")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </motion.table>
+      )}
       <div className="w-full h-[1px] text-[#E2E2EA] "></div>
       <div className="py-[20px] px-[24px] bg-white rounded-br-[12px] rounded-bl-[12px] flex items-center justify-between">
         <div>
@@ -96,7 +99,10 @@ const SchoolProject = () => {
         </div>
 
         <div>
-          <Pagination pageCount={pageCount} onPageChange={handlePageClick} />
+          <ProjectPagination
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+          />
         </div>
       </div>
     </div>
