7acb7c17b2b00fb0f2a640514f3920932f77e9ed
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-11-29 17:46:43 +0500

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ const securityHeaders = [
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXTAUTH_URL: "https://www.mkinfo.uz/",
-    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_URL: "https://www.mkinfo.uz",
+    // NEXTAUTH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=",
   },
   // async redirects() {
