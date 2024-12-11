cd3d2fa3e6ccf92cc64f641329ee2fba28dded6c
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-12-11 12:29:45 +0500

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: "https://backend.mkinfo.uz/",
+  API_URL: "https://backend-market.tmsiti.uz/",
   FILE_URL: "https://backend-market.tmsiti.uz/static/categories/materials/",
   DEFAULT_APP_LANG: "uz",
 };
