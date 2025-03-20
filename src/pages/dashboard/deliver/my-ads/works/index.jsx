807f9bdba93f2016bd0f7597102d80b317a5b598
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-03-20 09:54:57 +0500

    fixed

--- src/pages/dashboard/deliver/my-ads/works/index.jsx
+++ src/pages/dashboard/deliver/my-ads/works/index.jsx
@@ -13,8 +13,10 @@ import dayjs from "dayjs";
 import usePutQuery from "@/hooks/api/usePutQuery";
 import toast from "react-hot-toast";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 const MyMaterials = () => {
+  const { data: session } = useSession();
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(24);
   const [itemId, setItemId] = useState(null);
@@ -26,6 +28,10 @@ const MyMaterials = () => {
       page,
       page_size: pageSize,
     },
+    headers: { token: `${get(session, "user.token")}` },
+    enabled: !!(
+      get(session, "user.token") && get(session, "user.role") === "company"
+    ),
   });
 
   const { mutate: deactivateRequest, isLoading: isLoadingDeActivate } =
